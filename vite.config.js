d57dc30c949ec04e9eaab373aa4fb47f82de4b8d
ThibautSBN <tsembeni@myges.fr>
2022-09-29 23:48:35 +0200

    :tada: initial commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite"
+import { resolve } from "path"
+
+export default defineConfig({
+    root: resolve("src"),
+    server: {
+        port: 8000,
+        host: "0.0.0.0"
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true
+            },
+        }
+    },
+})
